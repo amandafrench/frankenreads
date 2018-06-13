102ecc666ab42a465b1c3bc6345e21b68ab12c9c
amandafrench <amanda@amandafrench.net>
2018-06-13 08:48:10 -0400

    Updates theme Avada

--- wp-content/themes/Avada/includes/upgrade/class-avada-upgrade-551.php
+++ wp-content/themes/Avada/includes/upgrade/class-avada-upgrade-551.php
@@ -74,6 +74,9 @@ class Avada_Upgrade_551 extends Avada_Upgrade_Abstract {
 				continue;
 			}
 
+			// Get language specific TOs.
+			$options = get_option( $this->option_name . '_' . $language, array() );
+
 			$options = $this->correct_chart_default_value( $options );
 			$options = $this->page_title_bar_hover( $options );
 
@@ -82,7 +85,7 @@ class Avada_Upgrade_551 extends Avada_Upgrade_Abstract {
 	}
 
 	/**
-	 * Corrects default Chart Gridline Gridline Color value.
+	 * Corrects default Chart Gridline Color value.
 	 *
 	 * @access private
 	 * @since 5.5.1
